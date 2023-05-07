4aa2cb2240d540ea83f78bcf46843ccca935f8ab
Pu-ck <polecjan@gmail.com>
2023-05-07 18:20:01 +0200

    books quantity display fix

--- src/main/webapp/libsystem-app/src/app/components/books/books.component.ts
+++ src/main/webapp/libsystem-app/src/app/components/books/books.component.ts
@@ -54,4 +54,9 @@ export class BooksComponent implements OnInit {
     this.router.navigate([`books/${id}`]);
   }
 
+  public getAffiliateBookQuantity(book: any, affiliateId: number): string {
+    const affiliateBook = book.affiliateBooks.find((ab: any) => ab.affiliateId === affiliateId);
+    return affiliateBook ? `${affiliateBook.currentQuantity}/${affiliateBook.generalQuantity}` : '';
+  }
+
 }
